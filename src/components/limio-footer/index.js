a8c33bd05af50fce6fc83420ff7e6666eb4255e5
Sergei Palmer <10038681+s-palmer@users.noreply.github.com>
2024-10-02 11:34:00 +0100

    chore: fix social links

--- src/components/limio-footer/index.js
+++ src/components/limio-footer/index.js
@@ -1,67 +1,97 @@
 // @flow
-import React from "react"
-import FooterNavigation from "./components/FooterNavigation"
-import ContactFields from "./components/ContactFields"
-import Logo from "./components/Logo"
-import SocialLinks from "./components/SocialLinks"
-import "./index.css"
+import React from "react";
+import FooterNavigation from "./components/FooterNavigation";
+import ContactFields from "./components/ContactFields";
+import Logo from "./components/Logo";
+import SocialLinks from "./components/SocialLinks";
+import "./index.css";
 
 const navigation = [
   {
-    "label": "Home",
-    "url": "/"
+    label: "Home",
+    url: "/",
   },
   {
-    "label": "Gift",
-    "url": "/gift"
+    label: "Gift",
+    url: "/gift",
   },
   {
-    "label": "Manage My Account",
-    "url": "/mma"
-  }
-]
+    label: "Manage My Account",
+    url: "/mma",
+  },
+];
 
 const contactFields = [
   {
-    "label": "Email:",
-    "value": "support@limio.com",
-    "url": "mailto:support@limio.com"
+    label: "Email:",
+    value: "support@limio.com",
+    url: "mailto:support@limio.com",
   },
   {
-    "label": "Telephone:",
-    "value": "020 1234 1234",
-    "url": "tel:020 1234 1234"
+    label: "Telephone:",
+    value: "020 1234 1234",
+    url: "tel:020 1234 1234",
   },
   {
-    "label": "Address:",
-    "value": "Leather Market, Weston Street, London, SE1 3ER",
-    "url": ""
-  }
-]
+    label: "Address:",
+    value: "Leather Market, Weston Street, London, SE1 3ER",
+    url: "",
+  },
+];
 
 const LimioFooter = () => {
-
   return (
     <div className="FooterOuter">
       <div className="FooterWrapper">
         <div className="FooterContent">
-          <Logo logo={"https://custom-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_300,w_300,f_auto,q_auto/1156184/637770_576700.png"} />
+          <Logo
+            logo={
+              "https://custom-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_300,w_300,f_auto,q_auto/1156184/637770_576700.png"
+            }
+          />
           <div className="FooterSections">
             <div className="FooterLeft">
               <ContactFields contactFields={contactFields} />
             </div>
             <div className="FooterRight">
-              <SocialLinks facebookLink={"https://www.facebook.com/people/Limio/100036084716440/"} twitterLink={"https://www.twitter.com/TryLimio"} instagramLink={"https://www.instagram.com/limio"} />
-              <a className="FooterLink" href={"https://www.limio.com/privacy"} target="_blank" rel="noopener noreferrer"><p>Privacy Policy</p></a>
-              <p>Copyright © 2019 Limio</p>
-              <p>Powered by{" "}<a id="powered-link" href={"https//www.limio.com"} target="_blank" rel="noopener noreferrer">Limio</a></p>
+              <SocialLinks
+                facebookLink={
+                  "https://www.facebook.com/people/Limio/100036084716440/"
+                }
+                twitterLink={"https://www.twitter.com/TryLimio"}
+                linkedInLink={"https://www.linkedin.com/company/limio"}
+              />
+              <a
+                className="FooterLink"
+                href={"https://www.limio.com/privacy"}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <p>Privacy Policy</p>
+              </a>
+              <p>© {new Date().getFullYear()} Limio</p>
+              <p>
+                Powered by{" "}
+                <a
+                  id="powered-link"
+                  href={"https//www.limio.com"}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Limio
+                </a>
+              </p>
             </div>
           </div>
         </div>
-        <FooterNavigation items={navigation} alignment={"center"} divider={"-"} />
+        <FooterNavigation
+          items={navigation}
+          alignment={"center"}
+          divider={"-"}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LimioFooter
+export default LimioFooter;
